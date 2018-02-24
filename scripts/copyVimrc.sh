973e9ebd7d5b49f81332aba25aaf253cc03f7a40
tic40 <ccpzjoh@gmail.com>
2018-02-24 11:58:38 +0900

    add install vim-plug

--- scripts/copyVimrc.sh
+++ scripts/copyVimrc.sh
@@ -2,3 +2,6 @@
 
 cp ../vim/.vimrc ~/
 cp -R ../vim/.vim ~/
+
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
