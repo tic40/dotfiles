6793d99047bd3dd2656b61399f3de90f45ef2fd5
tic40 <ccpzjoh@gmail.com>
2025-01-15 14:41:24 +0900

    Add ghosttty.sh

---
+++ scripts/ghostty.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p ~/.config/ghostty
+cp ../ghostty/config ~/.config/ghostty/
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 cd `dirname $0`
 
 scripts_dir='scripts'
-scripts=('gitconfig.sh' 'neovim.sh' 'sshconfig.sh' 'zsh.sh')
+scripts=('gitconfig.sh' 'neovim.sh' 'sshconfig.sh' 'zsh.sh' 'ghostty.sh')
 
 for ((i = 0; i < ${#scripts[@]}; i++)) {
   ${scripts_dir}/${scripts[i]}
