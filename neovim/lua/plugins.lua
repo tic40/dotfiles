f718e083d3fe24687b757282b1a89707fefb9c03
tic40 <ccpzjoh@gmail.com>
2025-12-17 21:41:36 +0900

    Update

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -13,6 +13,7 @@ return {
   -- Syntax highlight
   {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
     build = ':TSUpdate',
     config = function()
       require'nvim-treesitter.configs'.setup {
